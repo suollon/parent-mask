cd7efa892f67724cf7acd096fb402762c4f57435
wangwulei <wangwulei@18183g.com>
2020-11-20 13:12:04 +0800

    service-inland-bi接入nacos配置中心，naocs注册中心；

--- mask-service/service-inland-bi/src/main/java/com/lingdong/service/inland/bi/ServiceInlandBiApplication.java
+++ mask-service/service-inland-bi/src/main/java/com/lingdong/service/inland/bi/ServiceInlandBiApplication.java
@@ -2,7 +2,9 @@ package com.lingdong.service.inland.bi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceInlandBiApplication {
 
