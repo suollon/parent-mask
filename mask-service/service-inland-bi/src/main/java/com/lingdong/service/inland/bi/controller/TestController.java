7af07f4c2ca4dfebb4f77c51c9ca7a46794d0af4
wangwulei <wangwulei@18183g.com>
2020-11-23 10:23:44 +0800

    去除sentinel；

--- mask-service/service-inland-bi/src/main/java/com/lingdong/service/inland/bi/controller/TestController.java
+++ mask-service/service-inland-bi/src/main/java/com/lingdong/service/inland/bi/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.lingdong.service.inland.bi.controller;
 
-import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.lingdong.common.service.inland.bi.TestClient;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,7 +21,6 @@ public class TestController implements TestClient {
     private String userPassword;
 
     @GetMapping
-    @SentinelResource
     public String get(@RequestParam String name) {
         return "hello" + name + "===userName=" + userName + "======userPassword=" + userPassword;
     }
